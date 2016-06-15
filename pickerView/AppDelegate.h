6c58328e47ee0bb893f8d57825d4b49a8f602c8b
Patty <yu916700yupp@163.com>
2016-06-15 14:18:59 +0800

    Initial Commit

---
+++ pickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pickerView
+//
+//  Created by patty on 16/6/15.
+//  Copyright © 2016年 patty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pickerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pickerView
+//
+//  Created by patty on 16/6/15.
+//  Copyright © 2016年 patty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
